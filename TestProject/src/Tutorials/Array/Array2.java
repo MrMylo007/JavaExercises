ec1a2085338d768a27b5e29ffe4910128e2ccc37
user1 <johnnytran@192.168.0.130>
2018-08-27 21:09:27 +0200

    Array2 Push

--- TestProject/src/Tutorials/Array/Array2.java
+++ TestProject/src/Tutorials/Array/Array2.java
@@ -1,5 +1,7 @@
 package Tutorials.Array;
 
+import java.util.Scanner;
+
 public class Array2 {
 
 	public static void main(String[] args) {
@@ -18,6 +20,17 @@ public class Array2 {
 		// Merke: Index - Anzahl der Elemente -1 
 		// Index hier ist 0..5
 		
+		System.out.println("Geben Sie 6 Lottozahlen ein:");
+		for(int i = 0; i < lottoZahlen.length; i++) {
+			Scanner scan = new Scanner(System.in);
+			int zahlUser = scan.nextInt();
+			lottoZahlen[i] = zahlUser;
+		}
+		
+		//Output des User Inputs:
+		for(int i = 0; i < lottoZahlen.length; i++) {
+			System.out.println(lottoZahlen[i]);
+		}
 		
  	}
 
