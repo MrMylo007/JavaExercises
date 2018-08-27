a71b0ddb1467e89c88c40362a3d6a4c5458b5e4f
user1 <johnnytran@192.168.0.130>
2018-08-27 21:24:07 +0200

    Short Math Quiz

---
+++ TestProject/src/Tutorials/Array/Array3.java
@@ -0,0 +1,32 @@
+package Tutorials.Array;
+
+import java.util.Scanner;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Array Deklaration
+		// Datatyp[] Name = new Datentyp[Anzahl];
+		String[] fragen = {"Wie viel ist 5+4", "Wie viel ist 3*5"};
+		int [] antworten = {9,15};
+
+		int count = 0;
+		while (count < fragen.length) {
+			System.out.println(fragen[count]);
+			
+			Scanner scan = new Scanner(System.in);
+			int eingabeUser = scan.nextInt();
+			
+			if(eingabeUser == antworten[count]) {
+				System.out.println("Richtig");
+				count ++;
+			}
+			else {
+				System.out.println("falsch");
+			}
+		}
+	}
+
+}
