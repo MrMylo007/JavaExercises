e6cbd0704e31c3b72c79a986533bcdea12e2d8dd
user1 <johnnytran@192.168.0.130>
2018-08-27 22:37:47 +0200

    Variable Length Arguements

---
+++ TestProject/src/Arguments/VariableLengthArguments.java
@@ -0,0 +1,25 @@
+package Arguments;
+
+public class VariableLengthArguments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Variable Length Arguements
+		
+		System.out.println(avaerag(43,55,30,156));
+		System.out.println(avaerag(43,55,30,156,200,500));
+		System.out.println(avaerag(30, 40, 50));
+		
+	}
+	
+	// ... = dont know how many arguments
+	public static int avaerag (int...numbers) {
+		int total = 0;
+		for (int x:numbers) {
+			total+=x;
+		}
+		
+		return total/numbers.length;
+	}
+
+}
