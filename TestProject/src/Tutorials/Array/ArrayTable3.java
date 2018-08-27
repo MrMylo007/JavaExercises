e799735c65cab007452163688dced4ed069a6d66
user1 <johnnytran@192.168.0.130>
2018-08-27 22:03:58 +0200

    Add comments

--- TestProject/src/Tutorials/Array/ArrayTable3.java
+++ TestProject/src/Tutorials/Array/ArrayTable3.java
@@ -10,15 +10,20 @@ public class ArrayTable3 {
 		// https://youtu.be/pHxtKDENDdE
 		
 		Random rand = new Random();
+		//A new Array called freq, which has 7 Index
 		int freq[] = new int[7];
 		
+		//Roll the Dice 1000 Times
 		for(int roll = 1; roll < 1000; roll ++) {
+			//Everytime the dice hits a number between 1-6
+			//it stores and count the hits.
 			++freq[1 + rand.nextInt(6)];
 			
 		}
 
 		System.out.println("Face\tFrequency");
 		
+		//Basic Table
 		for (int face = 1; face < freq.length; face ++) {
 			System.out.println(face + "\t" + freq[face]);
 		}
