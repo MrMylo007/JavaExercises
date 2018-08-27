87d7c4d3f60bd100f2d09772779772e54efc7f7f
user1 <johnnytran@192.168.0.130>
2018-08-27 20:44:33 +0200

    New Array

---
+++ TestProject/src/Tutorials/WasIstEinArray.java
@@ -0,0 +1,32 @@
+package Tutorials;
+
+public class WasIstEinArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Variable Dekalartion
+		//Datentyp Name = Wert;
+		
+		int zahl = 50;
+		double zahl1 = 34.35;
+		
+		//Array Deklaration
+		//Datatyp[] Name = new Datatp[Anzahl];
+		int[] zahlArray = new int [7];
+		zahlArray[3] = 50;
+		zahlArray[5] = 500;
+		zahlArray[0] = 40;
+		
+		double[] zahlArray1 = new double[7]; //nur das array erstellen
+		zahlArray1[1] = 34.35;
+		zahlArray1[4] = 50.45;
+		
+		String[] personA = {"Peter " + " Mueller ", "35"};
+		System.out.println("Name:" + personA[0]);
+		
+		
+	}
+
+}
