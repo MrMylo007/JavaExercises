e8b7e9180cbe910f55a0c563a39c0b4a1a384992
user1 <johnnytran@192.168.0.130>
2018-08-27 23:05:15 +0200

    Time Class Tutorial (Military & American Regulatory Time)

--- TestProject/src/Time/Main.java
+++ TestProject/src/Time/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// https://youtu.be/E0BTAqIltFc
 		
 		time TimeObject = new time();
 		System.out.println(TimeObject.toMilitary());
 		
 		TimeObject.setTime(22, 47, 30);
 		System.out.println(TimeObject.toMilitary());
+		
+		TimeObject.setTime(1, 47, 30);
+		System.out.println(TimeObject.toString());
 
 	}
 
--- TestProject/src/Time/time.java
+++ TestProject/src/Time/time.java
@@ -6,7 +6,7 @@ public class time {
 	private int minute;
 	private int second;
 	
-
+ 
 	public void setTime(int h, int m, int s) {
 		hour = ((h > 0 && h < 24) ? h : 0);
 		minute = ((m > 0 && m < 60) ? m : 0);
@@ -15,6 +15,14 @@ public class time {
 	
 	public String toMilitary() {
 		return String.format("%02d:%02d:%02d", hour, minute, second);
+	}
+	
+	
+	// "%d:%02d:%02d %s", the "%d" is going to output an integer
+	// %02d is going to output an integer, but it HAS to be 2 DIGITS.
+	// so the means if you put in a 1, it is going to show up in the console as "01"
+	public String toString() {
+		return String.format("%d:%02d:%02d %s", ((hour==0 || hour==12) ?12:hour%12), minute, second, (hour < 12? "AM" : "PM"));
 	}
 
 }
