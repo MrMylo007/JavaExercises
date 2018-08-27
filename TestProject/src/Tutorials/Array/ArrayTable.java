413ea83d12371025b190a2ca72d7b502254151c0
user1 <johnnytran@192.168.0.130>
2018-08-27 21:46:03 +0200

    Add URL

--- TestProject/src/Tutorials/Array/ArrayTable.java
+++ TestProject/src/Tutorials/Array/ArrayTable.java
@@ -4,6 +4,7 @@ public class ArrayTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// https://youtu.be/nTF-RcgsV0E
 		System.out.println("Index\tValue");
 		
 		int arrayName[]= {21,12,18,43, 2,51};
