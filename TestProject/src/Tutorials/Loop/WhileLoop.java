2cbab0171f4694820c72790512e2495b52f83762
user1 <johnnytran@192.168.0.130>
2018-08-27 21:16:18 +0200

    WhileLoop

---
+++ TestProject/src/Tutorials/Loop/WhileLoop.java
@@ -0,0 +1,40 @@
+package Tutorials.Loop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Kontrollstruktur steuern den Programmablaufplan
+		
+		// while = solange
+		
+		/* Allgeminer Aufbau einer while-Schleife
+		 * 
+		 * while(Bedigung){
+		 * 
+		 * 	Anweisungsblock
+		 * 
+		 * }
+		 * 
+		 * Merke: Bedigung kann entweder true = wahr
+		 *        oder
+		 *        false = ncht wahr
+		 *        sein
+		 */
+		
+		int count = 1;
+//		while(count != 6) { //true
+//		System.out.println("Ich bin " + count);
+//	    }
+		
+		while(true) {
+			System.out.println("Hi");
+			if (count == 3) {  //true
+				break;
+			}
+			count ++;
+		}
+	}
+
+}
