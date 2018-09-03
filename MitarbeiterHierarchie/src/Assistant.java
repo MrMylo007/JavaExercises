0c83367816d74daa0aba66e034743c090edb9447
user1 <johnnytran@192.168.0.130>
2018-09-03 19:38:11 +0200

    New Java Class Assistant (Extends Emplyee) -Getter, Setter & default Constructor

---
+++ MitarbeiterHierarchie/src/Assistant.java
@@ -0,0 +1,22 @@
+
+public class Assistant extends Employee{
+	//Attribute
+	private int hoursPerWeek;
+
+	//Getter
+	public int getHoursPerWeek() {
+		return hoursPerWeek;
+	}
+
+	//Setter
+	public void setHoursPerWeek(int hoursPerWeek) {
+		this.hoursPerWeek = hoursPerWeek;
+	}
+	
+	//Konstructor
+	public Assistant() {
+		
+	}
+	
+	
+}
