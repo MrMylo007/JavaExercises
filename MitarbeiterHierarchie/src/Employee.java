cb15122af3a0f511664e3abf4296610f5dbbf957
user1 <johnnytran@192.168.0.130>
2018-09-03 19:33:40 +0200

    new Java Class - Employee Attribute emplyeeId

---
+++ MitarbeiterHierarchie/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee extends Person{
+
+	//Attribut
+	private long employeeId;
+
+	//Getter
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	//Setter
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	//Konstruktor
+	public Employee() {
+		
+	}
+	
+	
+	
+	
+}
