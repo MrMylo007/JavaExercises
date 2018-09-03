89208345d5df6ef40ae77a661fb7cf4676911b68
user1 <johnnytran@192.168.0.130>
2018-09-03 19:34:15 +0200

    -New getter and setter for firstName & lastName -Constructor

--- MitarbeiterHierarchie/src/Person.java
+++ MitarbeiterHierarchie/src/Person.java
@@ -1,4 +1,32 @@
 
 public class Person {
 
+	//Attribute
+	private String firstName;
+	private String lastName;
+
+
+	//Getter
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	
+	//Setter
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
+	//Konstruktor
+	public Person () {
+	}
+	
+	
 }
