cebd81822c8ea2aa1416847a781a58b9b9064a3e
user1 <johnnytran@192.168.0.130>
2018-09-03 20:19:24 +0200

    Getter & Setter for Java Class Bank

--- KontoEroeffnung/src/Bank.java
+++ KontoEroeffnung/src/Bank.java
@@ -1,4 +1,27 @@
 
 public class Bank {
 
+	//Attribut
+	private String BankName;
+	private int BankKredit;
+	
+	//Getter
+	public String getBankName() {
+		return BankName;
+	}
+	public int getBankKredit() {
+		return BankKredit;
+	}
+	
+	//Setter
+	public void setBankName(String bankName) {
+		BankName = bankName;
+	}
+	
+	public void setBankKredit(int bankKredit) {
+		BankKredit = bankKredit;
+	}
+
+	
+	
 }
