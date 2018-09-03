bad6e66451d34636f6c2ff9d51ba0709903ba5c0
user1 <johnnytran@192.168.0.130>
2018-09-03 20:07:31 +0200

    some testing

--- MitarbeiterHierarchie/src/Programm.java
+++ MitarbeiterHierarchie/src/Programm.java
@@ -4,6 +4,11 @@ public class Programm {
 	public static void main(String[] args) {
 		
 		Manager man1 = new Manager("Henrie", "Boss", 1, 40);
+		String firstname = man1.getFirstName();
+		int nrofEmployees = man1.getNumberOfEmployees();
+		System.out.println(firstname);
+		System.out.println(nrofEmployees);
+		
 		Assistant ass1 = new Assistant("Markus", "Assistent", 2, 50);
 	}
 
