87596a8a3e34adb6ef3ae0615b77af68784fe261
user1 <johnnytran@192.168.0.130>
2018-09-03 19:55:22 +0200

    Add new Main Class to test the java-classes

--- MitarbeiterHierarchie/src/Programm.java
+++ MitarbeiterHierarchie/src/Programm.java
@@ -3,7 +3,8 @@ public class Programm {
 
 	public static void main(String[] args) {
 		
-
+		Manager man1 = new Manager("Henrie", "Boss", 1, 40);
+		Assistant ass1 = new Assistant("Markus", "Assistent", 2, 50);
 	}
 
 }
