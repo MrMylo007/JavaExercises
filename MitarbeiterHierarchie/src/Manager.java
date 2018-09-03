c15dccab42972e95a652c510713046e68e9f1e26
user1 <johnnytran@192.168.0.130>
2018-09-03 19:36:27 +0200

    New Java-Class Manager (extends Employee) -Getter, Setter & Constructor

---
+++ MitarbeiterHierarchie/src/Manager.java
@@ -0,0 +1,22 @@
+
+public class Manager extends Employee{
+	//Attribute
+	private int numberOfEmployees;
+
+	//Getter
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	//Setter
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	
+	//Konstructor
+	public Manager() {
+		
+	}
+	
+	
+}
